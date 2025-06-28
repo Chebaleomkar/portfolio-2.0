24b92718c72326fe633f69a98267c7f06e5a14ea
Omkar Chebale <omkarchebale0@gmail.com>
2025-06-28 17:07:43 +0530

    update resume

--- app/omkar-chebale-resume/page.tsx
+++ app/omkar-chebale-resume/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-100 dark:bg-gray-900">
       <iframe
-        src="https://www.canva.com/design/DAGf6FaUSE4/2iF-COdumuUm4-kJ6vtoaA/view?embed"
+        src="/Omkar Chebale_resume.pdf"
         className="w-full h-screen"
         style={ { border: 'none' } }
         allowFullScreen
